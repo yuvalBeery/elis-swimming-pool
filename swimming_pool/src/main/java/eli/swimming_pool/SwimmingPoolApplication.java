1746a20a53c09770c4dcf2d69965111574bf3ac1
u9198081 <u9198081@bsmch.net>
2024-02-10 10:42:58 +0200

    add project template

---
+++ swimming_pool/src/main/java/eli/swimming_pool/SwimmingPoolApplication.java
@@ -0,0 +1,13 @@
+package eli.swimming_pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimmingPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimmingPoolApplication.class, args);
+	}
+
+}
---
+++ swimming_pool/src/test/java/eli/swimming_pool/SwimmingPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package eli.swimming_pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimmingPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
