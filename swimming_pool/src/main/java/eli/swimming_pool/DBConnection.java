a4c365dd9d1d5219bf9f9de987045ff42c3bbec2
u9198081 <u9198081@bsmch.net>
2024-02-10 11:02:46 +0200

    fix(DBConnection.java): change database name to postgres

--- swimming_pool/src/main/java/eli/swimming_pool/DBConnection.java
+++ swimming_pool/src/main/java/eli/swimming_pool/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/eli's pool";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER_NAME = "postgres";
     private static final String PASSWORD = "postgres";
 
